6d7f20fe538a823296840f8e232f10c43de81653
eeseohyun <jinlijinyoung@gmail.com>
2023-12-30 03:10:30 +0900

    UI: 로그아웃 버튼 추가

--- components/Header.vue
+++ components/Header.vue
@@ -2,17 +2,21 @@
   <div
     class="flex bg-[#5655E2] justify-end items-center mx-auto max-w-sm h-10 px-3 py-6"
   >
-    <UChip
-      v-for="({ name, icon, count, link }, idx) in items"
-      :key="idx"
-      color="red"
-      class="ml-2"
-    >
+    <template v-for="(item, idx) in items">
+      <UChip v-if="shouldRenderChip(item)" color="red" class="ml-2">
+        <UButton
+          :icon="item.icon"
+          class="bg-transparent text-white hover:bg-transparent p-0.5"
+        />
+      </UChip>
+
       <UButton
-        :icon="icon"
-        class="bg-transparent text-white hover:bg-transparent p-0.5"
+        v-else
+        :key="idx"
+        :icon="item.icon"
+        class="bg-transparent text-white hover:bg-transparent p-0.5 ml-2"
       />
-    </UChip>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
@@ -29,6 +33,18 @@ const items = [
     count: 3,
     link: 'alerts',
   },
+  {
+    name: 'logout',
+    icon: 'i-heroicons-power',
+    count: null,
+    link: 'logout',
+  },
 ];
+
+const shouldRenderChip = (item: any) => {
+  return (
+    item.count !== null && item.count !== undefined && item.count !== false
+  );
+};
 </script>
 <style></style>
