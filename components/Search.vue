50a5298eedd59b77fd49fed8d78a89f2176bdac1
eeseohyun <jinlijinyoung@gmail.com>
2023-12-30 03:11:25 +0900

    UI: Search 컴포넌트

---
+++ components/Search.vue
@@ -0,0 +1,23 @@
+<script setup>
+const search = async (q) => {
+  const users = await $fetch('baseUrl/friend/add', {
+    params: { q },
+  });
+
+  return users
+    .map((user) => ({ id: user.id, label: user.name, suffix: user.email }))
+    .filter(Boolean);
+};
+
+const selected = ref([]);
+</script>
+
+<template>
+  <USelectMenu
+    v-model="selected"
+    :searchable="search"
+    placeholder="친구 검색"
+    multiple
+    by="id"
+  />
+</template>
